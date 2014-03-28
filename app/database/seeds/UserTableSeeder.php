7a1bb454315ca1f957a49a1ecf68ed0eb65691ff
NathanielMedrano <ndmedrano@yahoo.com>
2014-03-28 15:36:53 -0500

    Blog
    Ready for deployment version 1

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('users')->delete();
+
+		$user = new User();
+		$user->email = 'natmed1991@gmail.com';
+		$user->password = Hash::make('letnatin');
+		$user->save();
+	}
+
+}
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -12,7 +12,7 @@
 	</style>
 
     @section('content')
-    
+
  	<div id='twitter'>
  		<a class="twitter-timeline" href="https://twitter.com/ndmedrano" data-widget-id="449618080012849152">Tweets by @ndmedrano</a>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
