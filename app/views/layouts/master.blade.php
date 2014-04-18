4d33e34652329ad34d8ecb47c6a997733813ebb9
NathanielMedrano <ndmedrano@yahoo.com>
2014-04-18 18:06:11 -0500

    Add address bar image

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>NatsTech - Home</title>
+    <title><img src="/img/bot.jpg" ></img>NatsTech</title>
 
      <!-- Latest compiled and minified CSS -->
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
