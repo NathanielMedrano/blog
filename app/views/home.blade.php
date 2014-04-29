24549f4eece45d6e982a04cca155ffa3c95966b4
NathanielMedrano <ndmedrano@yahoo.com>
2014-04-29 00:18:57 -0500

    Adjusted rows

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -18,15 +18,20 @@
 
     @section('content')
 
- 	<div id='twitter'>
- 		<a class="twitter-timeline" href="https://twitter.com/ndmedrano" data-widget-id="449618080012849152">Tweets by @ndmedrano</a>
-		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    <div class="row">
+    	<div class="col-md-9">
+		 	<div id='twitter'>
+		 		<a class="twitter-timeline" href="https://twitter.com/ndmedrano" data-widget-id="449618080012849152">Tweets by @ndmedrano</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			</div>
+		</div>
+		<div class="col-md-9">	
+			<div class="col-xs-6 col-md-4" id="logform">
+		    
+			   	<h2><a href="{{{ action('PostsController@show' , $post->id) }}} ">{{{ $post->title }}}</a></h2>
+			    <p>{{{ Str::words($post->body, 10) }}}</p>
+			</div>	
+		</div>
 	</div>
-<div class="col-xs-6 col-md-4" id="logform">
-    
-   	<h2><a href="{{{ action('PostsController@show' , $post->id) }}} ">{{{ $post->title }}}</a></h2>
-    <p>{{{ Str::words($post->body, 10) }}}</p>
-</div>
-
 
     @stop
